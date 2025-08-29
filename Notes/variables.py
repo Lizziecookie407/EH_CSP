10c2e4744cf501baf6728104b5be218cd06d215d
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-08-29 13:26:17 -0600

    Variables notes

---
+++ Notes/variables.py
@@ -0,0 +1,33 @@
+# EH 6th Varables Notes
+
+#Variable
+subject = "CSP"
+pi = 3.1415926535897932384626433832795028841971693993751
+color = "blue" 
+
+print(pi)
+
+#snake_case (all lower case, replace space with underscore)
+baseball_team = "Red Sox"
+#camelCase (no spaces, first letter of all words exept the first are capitalized)
+baseballTeam = "Pirates"
+#PascalCase (capitalize first of every word, no spaces)
+BaseballTeam = "Giants"
+
+#reassign a variable
+pi = 3.14159265
+
+pi = "French Silk"
+pi = "Pumpkin"
+print(pi)
+
+# input
+name = input("What is your name? ")
+color = input("What is your favoie color?\n")
+
+quest = input("What is your quest: ")
+
+print("What is your favorite animal?")
+answer = input()
+
+print("Your name is",name,"this is",subject,"the best pi is",pi,"the best color is",color,)
