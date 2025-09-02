be1baca49ff43753988a0153dae3d3d80bbe04b5
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-02 13:36:11 -0600

    Variables practice

---
+++ variables_practice.py
@@ -0,0 +1,16 @@
+#EH 6th Variables practice
+
+name= "Eliza"
+numberA= 9
+numberB= 999
+breakfast= "chocolate milk"
+color= "green"
+school= "UCAS"
+year= 2025
+eye_color= "brown"
+age= "5,477 days"
+subject= "seminary"
+
+print("My name is",name,"and it is",year,"so I am",age,"old.\n")
+print("My favorite color is",color,"and I had",breakfast,"for breakfast before coming to",school,".\n")
+print("My eyes are",eye_color,", my favorite subject is",subject,", and my two numbers are",numberA,"and",numberB,".")
