0daebe94ade360e94126315f884ad73e8d4cb470
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-04 13:43:07 -0600

    esspressisons

--- Notes/expressions.py
+++ Notes/expressions.py
@@ -1,1 +1,43 @@
 #EH 6th Expressions Notes
+
+#What is an algorithm? 
+    #A set of instructions to amcomplish a task. it will work every time and do the exact same steps.
+#List steps in an algorithm: 
+    #1) determine what variables/peices of into you need. 2) prosesses info. 3) provide output.
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+    #+ add, - subtract, * multiply, / devide, ** exponents, //integer division, % modulo(remainder), 
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+#Why are expressions so important in programming?
+    #
+# an expressison is a collection of numbers and symbols to get a result.
+
+name=input("What is your name? ")
+print("Hello,",name,"!")
+
+#algorithm for area
+length=5
+width=20
+area= length * width
+print("\n",length, "X", width, "=", area)
+
+#step 1
+age1=14
+age2=14
+age3=15
+age4=15
+#step 2
+average= (age1+age2+age3+age4)/4
+#step 3 
+print("\nThe average of", age1, ",", age2, ",", age3, ", and", age4, "is", average, ".")
+
+#math equations
+num_one =  10
+num_two = 6
+print("\naddition:",num_one + num_two)
+print("subtraction:",num_one - num_two)
+print("Multiplication:",num_one * num_two)
+print("Devision:",num_one / num_two)
+print("exponents:",num_one ** num_two)
+print("integer division:",num_one // num_two)
+print("modulo (remainder):",num_one % num_two)
