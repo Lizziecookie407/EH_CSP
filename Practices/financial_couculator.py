af28950b6c9488438138fa433aeca0947a4e9106
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-05 16:11:47 -0600

    financial

--- Practices/financial_couculator.py
+++ Practices/financial_couculator.py
@@ -9,5 +9,14 @@ transportation = 500
 
 savings = 300 #10 percent
 percent_rent = 1200/300
-total_expenses (rent + ) #everything plus everything 
-spending #income - total
+total_expenses= 9 #rent +  #everything plus everything 
+spending = 9 #income - total
+
+income = float(input("Your monthly income: "))
+rent = float(input("Your monthly rent: "))
+utilities = float(input("Your monthly utilities: "))
+goceries = float(input("Your monthly groceries: "))
+transportation = float(input("Your monthly transportation: "))
+
+print("\n\nIncome:",income)
+print("\nRent:",rent,",",percent_rent)
