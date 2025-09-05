271ef9f4a8f9fb482a01c989ba3de690a6853fb0
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-05 13:29:12 -0600

    expresisons notes

--- Notes/expressions.py
+++ Notes/expressions.py
@@ -5,12 +5,13 @@
 #List steps in an algorithm: 
     #1) determine what variables/peices of into you need. 2) prosesses info. 3) provide output.
 #List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
-    #+ add, - subtract, * multiply, / devide, ** exponents, //integer division, % modulo(remainder), 
+    #+ add, - subtract, * multiply, / devide, ** exponents, //integer division, % modulo(remainder) 
 #List ALL of the different assignment operators (Give me the symbols and what it does)
-    #
 #Why are expressions so important in programming?
-    #
-# an expressison is a collection of numbers and symbols to get a result.
+    #because if you set it up once, you never have to do it again. lazyness in advance. Long term.
+# an expressison is a collection of numbers and symbols that get a result.
+# integer is a data type that can hold whole numbers. can be negitive.
+# float is short for floating-point number and holds decimals. 
 
 name=input("What is your name? ")
 print("Hello,",name,"!")
@@ -31,13 +32,23 @@ average= (age1+age2+age3+age4)/4
 #step 3 
 print("\nThe average of", age1, ",", age2, ",", age3, ", and", age4, "is", average, ".")
 
-#math equations
-num_one =  10
-num_two = 6
-print("\naddition:",num_one + num_two)
-print("subtraction:",num_one - num_two)
-print("Multiplication:",num_one * num_two)
-print("Devision:",num_one / num_two)
-print("exponents:",num_one ** num_two)
-print("integer division:",num_one // num_two)
-print("modulo (remainder):",num_one % num_two)
+#math equations/assignment operators
+num_one =  float(input("Give me a number:\n"))
+num_two = int(input("Give me a number:\n"))
+
+num_one += num_two
+print("\naddition (+):",round(num_one, 0)) #Rounding: 1. what needs rounded 2. by how manyhg
+num_one -= num_two
+print("subtraction (-):",num_one)
+num_one *= num_two
+print("multiplication (*):",num_one)
+num_one /= num_two
+print("devision (/):",round(11/3, 0))
+num_one **= num_two
+print("exponents (**):",num_one)
+num_one //= num_two
+print("integer division (//):", num_one)
+num_one %= num_two
+print("modulo (remainder) (%):",num_one)
+
+print("\n(3*5**2/15)-(5-2**2)=",(3*5**2/15)-(5-2**2))
