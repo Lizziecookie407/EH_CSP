f8585c990f5460cb30cad9bb3b5a287e845a23a6
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-05 13:44:15 -0600

    expresssoinsss

---
+++ Practices/financial_couculator.py
@@ -0,0 +1,13 @@
+#EH 6th Financial Calculator
+#save money as float rounded to two decimals
+
+income = 3000
+rent = 1200
+utilities = 250
+groceries = 200
+transportation = 500
+
+savings = 300 #10 percent
+percent_rent = 1200/300
+total_expenses (rent + ) #everything plus everything 
+spending #income - total
