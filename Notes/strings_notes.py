97da0426232ef7b7d0550c90f452424619508fe8
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-09 13:03:23 -0600

    debugging def

--- Notes/strings_notes.py
+++ Notes/strings_notes.py
@@ -2,13 +2,13 @@
 
 print("I did it!")
 
-#What makes something a string? a collection of symbols that are held together by quotation marks or single quotes. both sides have to match.
+#What makes something a string? a string is a collection of symbols that are held together by quotation marks or single quotes. both sides of quotes have to match.
 #Why do we have strings? giving information, user inputs, words
 #How do stupid proofing and sanitization relate to each other? users=stupid
     #strip gets rid of spaces at begining and at the end
     #.upper and .lower makes all letters that case. if you do both the last one is what happens
 #Concatination: puts things right next to each other exactly as they are. use + (instead of comma)
-#What is debugging?
+#What is debugging? debudding is fixing problems in my code.
 #How do you debug the different types of errors?
 #Describe what each of the methods below does:
     #find()
