905f83adb688ec06de560e1ce5ee3533304118db
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-11 03:19:31 +0000

    silly silly silly

--- Practices/silly_sentances.py
+++ Practices/silly_sentances.py
@@ -1,6 +1,6 @@
 #EH 6th Silly Sentances
 
-print("Hello and welcome to my silly sentances generator!")
+print("Hello and welcome to my silly sentance generator!")
 
 word_one=input("Give me an animal: ").strip().lower()
 word_two=input("Give me a food item: ").strip().lower()
@@ -9,5 +9,5 @@ word_four=input("Give me a verb (ending in -ing): ").strip().lower()
 
 print("\nHere is your silly sentance!")
 print("If you give a "+word_one+" a "+word_two+", then it will ask you for a "+word_three+".")
-print("If you give the "+word_one+" a "+word_three+", then it will ask you to go "+word_four+" with it...")
-print("And before you know it, you'll be "+word_four+" with a "+word_one+", while eating a "+word_two+"!")
+print("If you give the "+word_one+" a "+word_three+", then you'll have to go "+word_four+" with them.")
+print("And before you know it, you'll be "+word_four+" while eating a "+word_two+" with a "+word_one+"!")
