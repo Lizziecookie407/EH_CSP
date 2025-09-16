77b4db0443e3e33305fcf4c983d863a56a1b4016
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-16 13:20:28 -0600

    saja boyzzzz

---
+++ Practices/update_hello.py
@@ -0,0 +1,15 @@
+#EH 6th Updated Hello World
+
+def welcome(name):
+    print(f"Hello, {name}!")
+
+name= "Jinu"
+welcome(name)
+name= "Abby"
+welcome(name)
+name= "Mystery"
+welcome(name)
+name= "Baby"
+welcome(name)
+name= "Romance"
+welcome(name)
