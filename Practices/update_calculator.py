20441a37a93bd51ff82abcf0397e3e818abdba53
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-16 22:47:35 +0000

    finished calc again

--- Practices/update_calculator.py
+++ Practices/update_calculator.py
@@ -4,10 +4,9 @@ print("Hi, welcome to my financial calculator! Put in each amount as a plain num
 
 def input_amount(amount):
     return float(input(f"Your monthly {amount}: "))
-    
 
 def percent_amount(amount):
-    round(amount/income*100,0)
+    return round(amount/income*100,0)
 
 income= input_amount("income")
 rent= input_amount("rent")
@@ -18,9 +17,17 @@ savings= income*.1
 total_expenses = rent+utilities+groceries+transportation+savings
 spending = income-total_expenses
 
-percent_savings = percent_amount("savings")
-percent_rent = percent_amount("rent")
-percent_utilities = percent_amount("utilities")
-percent_groceries = percent_amount("groceries")
-percent_transportation = percent_amount("transportation")
-percent_spending = percent_amount("spending")
+percent_savings = percent_amount(savings)
+percent_rent = percent_amount(rent)
+percent_utilities = percent_amount(utilities)
+percent_groceries = percent_amount(groceries)
+percent_transportation = percent_amount(transportation)
+percent_spending = percent_amount(spending)
+
+print("\nYour rent is $", rent, "and that is", percent_rent, "% of your income.")
+print("Your utilities are $", utilities, ", and that is", percent_utilities, "% of your income.")
+print("Your groceries are $", groceries, ", and that is", percent_groceries, "% of your income.")
+print("Your transportation is $", transportation, ", and that is", percent_transportation, "% of your income.")
+print("You should save $", savings, "a month, and that is", percent_savings, "% of your income.")
+print("You have $", spending, "of money left to spend each month, and that is",percent_spending, "% of your income.")
+print("Thanks for using my calculator!")
