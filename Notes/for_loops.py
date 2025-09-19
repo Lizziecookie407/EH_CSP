403fe037a08b4893f5a3718d5ba64104da438be5
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-09-19 13:30:03 -0600

    timmeeee

---
+++ Notes/for_loops.py
@@ -0,0 +1,13 @@
+# EH 6th Time and For Loops notes
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+#print(f"The time in seconds since Jan, 1 1970 is {epoch}")
+print(f"The current time is: {current}")
+print(f"The hour is {hour}")
+
+# Lists - store multiple pices of information in one varilable. Surrounded by straight brackets. Must all be correct datatype.
+siblings = ["Valerie", "Henry", "Lilly", ["Luna", "Zara", "Juno"], 3.141592653589]
--- Practices/time_of_day.py
+++ Practices/time_of_day.py
@@ -2,6 +2,15 @@
 
 time=int(input("What is the hour in military time: "))
 
-if time >= 1 and time <= 11:
-    if time
+if time >= 0 and time <= 4:
+    print("Good morning...")
+elif time >= 5 and time <= 11:
     print("Good morning!")
+elif time >11 and time <= 16:
+    print("Good afternoon!")
+elif time >=17 and time<=24:
+    print("Good evening!")
+elif time > 24:
+    print("Hey you, enter a valid time!")
+else:
+    print("Good day!")
