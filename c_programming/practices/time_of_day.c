d33d4936f75d35553cf0350ef1aea6ea8474ea1f
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-10-10 13:31:37 -0600

    timeszzz

--- c_programming/practices/time_of_day.c
+++ c_programming/practices/time_of_day.c
@@ -3,7 +3,22 @@
 #include <stdio.h>
 
 int main(void){
+    int time;
+    printf("What is the hour in military time: ");
+    scanf("%d", &time);
 
-    
+    if(time >= 0 && time <= 4){
+    printf("Good morning...");
+    }else if(time >= 5 && time <= 11){
+    printf("Good morning!");
+    }else if(time >11 && time <= 16){
+    printf("Good afternoon!");
+    }else if (time >=17 && time<=24){
+    printf("Good evening!");
+    }else if(time > 24 || time < 0){
+    printf("Hey you, enter a valid time!");
+    }else{
+    printf("Good day!");
+    }
     return 0;
 }
