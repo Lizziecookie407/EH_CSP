b764872488942ce08ca7311f4764ee50cc1a6662
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-10-13 13:44:31 -0600

    familyzzzzz

---
+++ c_programming/practices/family_loop.c
@@ -0,0 +1,14 @@
+// EH 6th
+
+#include <stdio.h>
+
+int main(void){
+
+    char family[7][20] = {"Mommy", "Daddy", "Eliza", "Valerie", "Henry", "Lilly", "Luna"};
+
+    for(int i = 0; i < 5; i++){
+    printf("Hello, {person}");
+    }
+    return 0;
+}
+
