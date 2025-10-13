0add668add9e2cca27b6054d5d36742ea562a0d8
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-10-13 13:28:10 -0600

    loooppppppzzz

--- c_programming/notes/loops_notes.c
+++ c_programming/notes/loops_notes.c
@@ -1,24 +1,53 @@
-// EH 6th Loops Notes
+// EH 6th Loops amd Arrays Notes
 /*
 What is a loop? 
- 
+ something that repetes when you tell it to, with iterators
 What are the two types of loops?
- 
-What is iteration
- 
+ for loop and while loop
+What is iteration?
+ a number counting a amount of times you loop
 What are arrays? 
- 
-How do you make lists in C? 
- 
+ a collection of data grouped together (a list)
 How do you make for loops in C? 
  
-How do you make while loops in C?*/
+How do you make while loops in C?
+*/
 
 
 #include <stdio.h>
 
 int main(void){
+/*
+    char *fruits[] = {"apple", "banana", "cherry"};
+    int num_fruits = sizeof(fruits) / sizeof(fruits[0]);
 
-    
+    for(int f 1 = 0; f < num_fruits; f++){
+        printf("I like %s!\n",fruits[f]);
+    }
+*/
+    //list or array
+    int nums[10] = {1,2,3,4,5,6,7,8,9,10}; //must set data type, braces instead of brackets, commas
+    char candy[5][20] = {"Skittles", "Butterfinger", "Reese's", "Kit Kat", "Twix"};
+
+    for(int x = 0; x < 10; x++){
+        printf("%d\n", nums[x]);
+    }
+
+    for(int i = 0; i < 5; i++){
+    printf("%s is my favorite candy!\n", candy[i]);
+    }
+
+    for(int num =1; num <11; num++){
+        printf("%d\n", num);
+    }
+
+    int goose = 6;
+    int count = 0;
+    while(count != goose){ //only difference from python is the parenthases and braces
+        printf("Duck\n");
+        count++;
+    }
+    printf("Goose!");
+
     return 0;
 }
