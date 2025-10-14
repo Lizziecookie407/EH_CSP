62c3f8ea00d881c27abe8fd3eb6f9362af826857
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-10-14 00:00:14 +0000

    finish family loop

--- c_programming/practices/family_loop.c
+++ c_programming/practices/family_loop.c
@@ -6,8 +6,8 @@ int main(void){
 
     char family[7][20] = {"Mommy", "Daddy", "Eliza", "Valerie", "Henry", "Lilly", "Luna"};
 
-    for(int i = 0; i < 5; i++){
-    printf("Hello, {person}");
+    for(int i = 0; i < 7; i++){
+    printf("Hello, %s!\n",family[i]);
     }
     return 0;
 }
