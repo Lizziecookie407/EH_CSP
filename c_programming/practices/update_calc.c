d59ace60ab2fc97f97146c59e416a7d2c93d8a6a
Lizziecookie407 <eliza.hyatt@ucas-edu.net>
2025-10-15 13:15:18 -0600

    a mystery

---
+++ c_programming/practices/update_calc.c
@@ -0,0 +1,9 @@
+// EH 6th Update Financial Calculator
+
+#include <stdio.h>
+
+int main(void){
+
+    
+    return 0;
+}
---
+++ c_programming/practices/update_hello.c
@@ -0,0 +1,20 @@
+// EH 6th Update Hello World
+
+#include <stdio.h>
+
+void welcome(char* name){
+    printf("Hello, %s!\n",name);
+
+}
+
+
+int main(void){
+    welcome("Jinu");
+    welcome("Abby");
+    welcome("Mystery");
+    welcome("Baby");
+    welcome("Romance");
+
+    
+    return 0;
+}
